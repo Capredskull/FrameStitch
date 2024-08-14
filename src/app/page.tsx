177768ff950f2f8a9181dd331aa43a034750e4e8
Capredskull <samyogg91@gmail.com>
2024-08-14 12:51:29 +0200

    Added footer and finished up the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Reviews } from "@/components/Reviews";
+import { buttonVariants } from "@/components/ui/button";
 import WallFrame from "@/components/WallFrame";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check,  Star } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -125,6 +127,51 @@ export default function Home() {
       <Reviews/>
       </div>
       </section>
+      <section>
+      <MaxWidthWrapper className ="py-24">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto mx-w-2xl sm:text-center">
+          <h2 className="order-1 mt-2 !leading-tight tracking-tight text-center text-balance text-gray-900 font-bold text-5xl md:text-6xl">Upload your own <span className="relative px-2">custom <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-400"/></span> images now {' '}</h2>
+          </div>
+        </div>
+        <div className="mx-auto max-w-6xl px-6 lg:px-10">
+          <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+            <img src="arrow.png" className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 md:rotate-0 rotate-90 "/>
+            <div className="relative h-auto w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+            <img src="tes-5.jpg" className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"/>
+            </div>
+            <WallFrame className="w-60" imgSrc="/tes-5.jpg"/>
+          </div>
+        </div>
+        <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+          <li className="w-fit">
+            <Check className="text-green-500 h-5 w-5 inline mr-1.5"/>
+            Recycled Material
+          </li>
+          <li className="w-fit">
+            <Check className="text-green-500 h-5 w-5 inline mr-1.5"/>
+            Build to last
+          </li>
+          <li className="w-fit">
+            <Check className="text-green-500 h-5 w-5 inline mr-1.5"/>
+            100+ frame choice
+          </li>
+          <li className="w-fit">
+            <Check className="text-green-500 h-5 w-5 inline mr-1.5"/>
+            Life time warranty
+          </li>
+          <div className="flex justify-center">
+            <Link href="/configure/upload" className={buttonVariants({
+              size:"lg",
+              className:"mx-auto mt-8"
+            })}>
+              Create Now <ArrowRight className="h-4 w-4 ml-1.5"/>
+            </Link>
+          </div>
+        </ul>
+      </MaxWidthWrapper>
+      </section>
+     
     </div>
   );
 }
